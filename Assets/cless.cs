69c3f19b8e4774694e747d4170b74741e4b4fbc0
blue90518 <a0906383384@gmail.com>
2025-01-08 20:40:20 +0800

    Initial commit

---
+++ Assets/cless.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cless : MonoBehaviour
+{
+    public int coin =50;
+    private int leve1=3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
