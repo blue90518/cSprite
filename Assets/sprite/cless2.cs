1677ff58ae2ced4a679fa5e71ba0a1297f91cc9e
blue90518 <a0906383384@gmail.com>
2025-01-08 21:44:39 +0800

    C#資料認識

---
+++ Assets/sprite/cless2.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cless2 : MonoBehaviour
+{
+    public int count=7;
+    public string character="彼岸";
+    public float turnspeed = 5.2f;
+    public float movespeed = 38.4f;
+    public bool isdead=false;
+    public bool gameover = true;
+    /// <summary>
+    /// 整數資料類型
+    /// </summary>
+    public byte lv=16;
+    public uint coin = 996;
+    public long items=30000;
+    /// <summary>
+    /// 字串與字元
+    /// </summary>
+    public string player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
